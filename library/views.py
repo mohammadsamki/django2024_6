35fcda137c49a8150ad46fd83ee7cc4e9cb6a950
mohammed alsamaki <samkimohd@gmail.com>
2024-11-09 14:38:42 +0300

    sussion

--- library/views.py
+++ library/views.py
@@ -11,19 +11,33 @@ def home(request):
     # get all the data in this modale
     users = Users.objects.all()
     print(users)
+    #  create a sussion that show the user how many he view the home page
+    #  create a variable that store the number of views
+    number_of_views = request.session.get('number_of_views', 0)
+    #  increment the number of views
 
+    request.session['number_of_views'] = number_of_views + 1
+    change_logo = request.session.get('change_logo', 0)
+    if request.method == 'POST':
+        print('post request')
+        request.session['change_logo'] = change_logo + 1
+
+        pass
     context = {
         'name':name,
         "worked":True,
-        "users":users
-
+        "users":users,
+        "number_of_views":number_of_views,
+        "change_logo":change_logo
     }
 
     return  render(request, 'home.html', context=context)
 
+
 def news(request):
     return  render(request, 'news.html')
 
+
 def slider(request):
-    return  render(request,'slider.html')
+    return  render(request, 'slider.html')
 
